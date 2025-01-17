ba054e44da3214f80c1bdcb93512a2f42f3e9efb
jazad136 <1129384+jazad136@users.noreply.github.com>
2025-01-17 13:45:29 -0500

    Add ASimplePlaywrightTest2.java

--- src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest2.java
+++ src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest2.java
@@ -1,6 +1,7 @@
 package com.serenitydojo.playwright;
 
 import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 import org.junit.jupiter.api.Assertions;
@@ -39,4 +40,29 @@ public class ASimplePlaywrightTest2 {
         browser.close();
         playwright.close();
     }
+
+    @Test
+    void shouldSearchByKeyword2() {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch();
+        Page page = browser.newPage();
+
+        page.navigate("https://practicesoftwaretesting.com");
+        page.locator("[placeholder=Search]");
+        System.out.println("TEST PASSED: Page contains field with 'search' placeholder");
+        Locator locSearch = page.locator("[id=search-query]");
+        System.out.println("TEST PASSED: Page contains clickable search button");
+
+        locSearch.fill("pliers");
+        System.out.println("TEST PASSED: Page contains editable field 'search-query'");
+
+        var loc = page.locator("button:has-text('Search')");
+        loc.click();
+        System.out.println("TEST PASSED: Button was clicked");
+        int matchingSearchResults = page.locator(".card").count();
+        System.out.println("Matching search results: " + matchingSearchResults);
+        browser.close();
+        playwright.close();
+    }
+
 }
